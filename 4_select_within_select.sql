fffe515b96af4640dcf8e5c91932301552b407cd
Dan Hong <dnaeye@gmail.com>
2019-07-30 11:55:31 -0700

    Update 4_select_within_select.sql
    Added answers for Q9 and Q10.

--- 4_select_within_select.sql
+++ 4_select_within_select.sql
@@ -86,6 +86,25 @@ WHERE name <= ALL (
 Find the continents where all countries have a population <= 25000000. 
 Then find the names of the countries associated with these continents. Show name, continent and population. */
 
+SELECT name, continent, population
+FROM world x
+WHERE 25000000 > ALL (
+  SELECT population 
+  FROM world y 
+  WHERE x.continent = y.continent 
+    AND y.population > 0
+)
+    
 /* Q10
 Some countries have populations more than three times that of any of their neighbours (in the same continent). 
 Give the countries and continents. */
+  
+SELECT name, continent
+FROM world x
+WHERE population > ALL (
+  SELECT population * 3
+  FROM world y
+  WHERE x.continent = y.continent 
+    AND population > 0 
+    AND y.name <> x.name
+)
